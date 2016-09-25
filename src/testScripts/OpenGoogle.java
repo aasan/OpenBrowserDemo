daea03d6287a9be1e6eb36ac7d5620c1f31ebf32
aasan <asjadtariq@yahoo.com>
2016-09-25 20:10:51 +0100

    source file adding

---
+++ src/testScripts/OpenGoogle.java
@@ -0,0 +1,17 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OpenGoogle {
+  @Test
+  public void f() throws InterruptedException {
+	  WebDriver driver = new FirefoxDriver();
+	  driver.get("http://google.com");
+	  driver.findElement(By.id("lst-ib")).sendKeys("Steve Jobs");
+	  Thread.sleep(5000);
+	  driver.close();
+  }
+}
