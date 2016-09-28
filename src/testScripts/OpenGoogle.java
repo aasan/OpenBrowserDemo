2bcd3142bca25997b8f787825b68bcf275cb0b50
aasan <asjadtariq@yahoo.com>
2016-09-28 10:23:26 +0100

    Updating WIth SauceLab Code

--- src/testScripts/OpenGoogle.java
+++ src/testScripts/OpenGoogle.java
@@ -6,9 +6,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class OpenGoogle {
+	public static final String USERNAME = "atariq";
+  public static final String ACCESS_KEY = "cd001cac-fd15-40fc-9925-566a691f4e06";
+  public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+	
   @Test
-  public void f() throws InterruptedException {
-	  WebDriver driver = new FirefoxDriver();
+  public void f() throws InterruptedException { DesiredCapabilities caps = DesiredCapabilities.safari();
+	  caps.setCapability("platform", "OS X 10.9");
+	  caps.setCapability("version", "7.0");
+	  
+    WebDriver driver = new RemoteWebDriver(new URL(URL), caps););
 	  driver.get("http://google.com");
 	  driver.findElement(By.id("lst-ib")).sendKeys("Jobs");
 	  Thread.sleep(5000);
